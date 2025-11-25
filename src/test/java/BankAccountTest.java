10f26b2bbb1b2372a036b446b5db403304591d33
IREStephen <g00465649@atu.ie>
2025-11-25 20:09:15 +0000

    Added deposit tests and implemented deposit()

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,18 +1,14 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+@Test
+public void testDepositIncreasesBalance() {
+    BankAccount account = new BankAccount("123", "John Doe", 100.0);
+    account.deposit(50.0);
+    assertEquals(150.0, account.getBalance());
+}
 
-public class BankAccountTest {
-
-    @Test
-    public void testConstructorStoresAccountNumber() {
-        BankAccount account = new BankAccount("123", "John Doe", 100.0);
-        assertEquals("123", account.getAccountNumber());
-    }
-
-    @Test
-    public void testConstructorStoresOwnerNameAndBalance() {
-        BankAccount account = new BankAccount("123", "John Doe", 100.0);
-        assertEquals("John Doe", account.getOwnerName());
-        assertEquals(100.0, account.getBalance());
-    }
+@Test
+public void testDepositZeroOrNegativeThrowsException() {
+    BankAccount account = new BankAccount("123", "John Doe", 100.0);
+    assertThrows(IllegalArgumentException.class, () -> account.deposit(0));
+    assertThrows(IllegalArgumentException.class, () -> account.deposit(-20));
+}
 }
